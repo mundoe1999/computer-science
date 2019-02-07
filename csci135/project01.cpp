5794d273eec30a18866073862f6f9bddfd4469a7
Esteban Mundo <33094840+mundoe1999@users.noreply.github.com>
2019-02-06 23:59:40 -0500

    Update project01.cpp
    Style polishing.

--- csci135/project01.cpp
+++ csci135/project01.cpp
@@ -3,73 +3,63 @@ Name: Esteban Mundo
 Class: CSCI 135
 Task D: Calculating Multiple Formulas, with exponents
 
-Calculates the value of the formulas, also includes exponents
+Calculates the sum of multiple formulas of a .txt file, with
+added implementation to take into account Exponents.
 */
+
 #include <iostream>
-
 using namespace std;
-
 
 int main(){
 	//Reading the .txt File
 	char s;
 
 	int sum = 0;			//Used to calculate the total Sum
-	int Value = 0;			//Used to store the numbers
-	char operation = '0';	//Checks the the operator, if it's (+) or (-)
-	char exponent = '0';
+	int num = 0;			//Used to store the numbers
+	char operation = '0';		//Checks the the operator, if it's (+) or (-)
 
 	while(cin >>  s) {
-
-		//What this does is that it checks if it is an operator
-		//If not, the base value is multiplied by 10 (since we are reading left to right)
-		//((s-48)%11) calculates whether the char is either 0-9, and adds it into the Value
+		//Checks if the character is a valid operator (excluding exponent)
+		//If not, the base value is multiplied by 10 (Since this is a Decimal-based system)
+		//((s-48)%10) calculates the appropiate decimal value and adds to num
 		if(s != '+' && s != '-' && s != ';'){
-			if(s == '^'){
-				Value = Value*Value;
-			}
-			else{
-				Value = Value*10 + ((s - 48)%11);
-			}
-
+			if(s == '^'){num = num*num;}
+			else{num = num*10 + ((s - 48)%10);}
 		}
 
 		//Sees whether the operator is (+) or (-)
 		else if(s == ';'){
 			if(sum == 0 && operation == '0'){
-				sum+=Value;
+				sum += num;
 			}
 			else if(operation == '+'){
-				sum+= Value;
+				sum += num;
 			}
 			else if(operation == '-'){
-				sum-= Value;
+				sum -= num;
 			}
-
+			
 			cout << sum << endl;
-			Value = 0;
+			num = 0;
 			sum = 0;
 			operation = '0';
 		}
 
 		else{
-
 			if(sum == 0 && operation == '0'){
-				sum+=Value;
+				sum += num;
 			}
 			else if(operation == '+'){
-				sum+= Value;
+				sum += num;
 			}
 			else if(operation == '-'){
-				sum-= Value;
+				sum -= num;
 			}
-
-			operation = s;
 			
-			//Reset the String to store the next sum of numbers
-			Value= 0;
+			//Num is reset, and sets operation with new operator
+			operation = s;
+			num = 0;
 		}
 	}
-
 	return 0;
 }
