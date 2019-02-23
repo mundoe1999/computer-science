22011a10db7e815e1902df3febf4cdf77a71b7bb
Esteban Mundo <33094840+mundoe1999@users.noreply.github.com>
2019-02-23 17:58:20 -0500

    Update Formula_Calculator.cpp

--- csci135/Formula_Calculator.cpp
+++ csci135/Formula_Calculator.cpp
@@ -11,63 +11,40 @@ using namespace std;
 
 
 int main(){
-	//Reading the .txt File
-	char s;
-
-	int sum = 0;			//Used to calculate the total Sum
-	int Value = 0;			//Used to store the numbers
+    //Reading the .txt File
+    char s;
+	
+    int sum = 0;		//Used to calculate the total Sum
+	int Value = 0;		//Used to store the numbers
 	char operation = '0';	//Checks the the operator, if it's (+) or (-)
-	char exponent = '0';
 
 	while(cin >>  s) {
-
-		//What this does is that it checks if it is an operator
+        //What this does is that it checks if it is an operator
 		//If not, the base value is multiplied by 10 (since we are reading left to right)
 		//((s-48)%11) calculates whether the char is either 0-9, and adds it into the Value
 		if(s != '+' && s != '-' && s != ';'){
-			if(s == '^'){
-				Value = Value*Value;
-			}
-			else{
-				Value = Value*10 + ((s - 48)%11);
-			}
-
+			if(s == '^'){Value = Value*Value;}
+			else{Value = Value*10 + ((s - 48)%10);}
 		}
-
-		//Sees whether the operator is (+) or (-)
 		else if(s == ';'){
-			if(sum == 0 && operation == '0'){
-				sum+=Value;
-			}
-			else if(operation == '+'){
-				sum+= Value;
-			}
-			else if(operation == '-'){
-				sum-= Value;
-			}
+            if(sum == 0 && operation == '0'){sum+=Value;}
+			else if(operation == '+'){sum+= Value;}
+			else if(operation == '-'){sum-= Value;}
 
+            //Print and Reset variables
 			cout << sum << endl;
 			Value = 0;
 			sum = 0;
 			operation = '0';
 		}
-
 		else{
+			if(sum == 0 && operation == '0'){sum+=Value;}
+			else if(operation == '+'){sum+= Value;}
+			else if(operation == '-'){sum-= Value;}
 
-			if(sum == 0 && operation == '0'){
-				sum+=Value;
-			}
-			else if(operation == '+'){
-				sum+= Value;
-			}
-			else if(operation == '-'){
-				sum-= Value;
-			}
-
-			operation = s;
-			
+			operation = s;	
 			//Reset the String to store the next sum of numbers
-			Value= 0;
+			Value = 0;
 		}
 	}
 
