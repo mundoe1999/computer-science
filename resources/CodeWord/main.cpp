ec3ca0d89c4c74cb36da873b674f1a48798cd6a0
mundoe1999 <estebanmundo1999@gmail.com>
2020-02-14 18:38:34 -0500

    Cleaned junk files

--- resources/CodeWord/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "CodeWordGenerator.hpp"
-using namespace std;
-
-int main(){
-  CodeWordGenerator test;
-  cout << test.generateLengthWord("[A-Z]+ABA[A-Z]*",6) << '\n';
-}
